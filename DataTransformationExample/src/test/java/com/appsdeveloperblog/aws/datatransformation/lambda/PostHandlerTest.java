b81db676b9704abaa874a986bb7d29d418c3b5e3
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-10-18 12:39:21 -0400

    Initial commit

---
+++ DataTransformationExample/src/test/java/com/appsdeveloperblog/aws/datatransformation/lambda/PostHandlerTest.java
@@ -0,0 +1,23 @@
+package com.appsdeveloperblog.aws.datatransformation.lambda;
+
+import com.appsdeveloperblog.aws.datatransformation.lambda.PostHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class PostHandlerTest {
+  @Test
+  public void successfulResponse() {
+//    PostHandler app = new PostHandler();
+//    APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
+//    assertEquals(result.getStatusCode().intValue(), 200);
+//    assertEquals(result.getHeaders().get("Content-Type"), "application/json");
+//    String content = result.getBody();
+//    assertNotNull(content);
+//    assertTrue(content.contains("\"message\""));
+//    assertTrue(content.contains("\"hello world\""));
+//    assertTrue(content.contains("\"location\""));
+  }
+}
